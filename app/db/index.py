f31b0bcdc6a0213e7a0c41f03ce046dfc148af98
edson costa <edsonm737@gmail.com>
2025-12-05 12:37:53 -0300

    Implementado as funçoes que trarao as informaçoes da paginas

---
+++ app/db/index.py
@@ -0,0 +1,11 @@
+def index_page():
+    return  [{"titulo": "Home"}]
+
+def perfil_modelos():
+
+    return[
+
+        {"nome": "Modelo 1", "descricao": "Descrição do modelo 1", "imagem": "modelo1.jpg"},
+        {"nome": "Modelo 2", "descricao": "Descrição do modelo 2", "imagem": "modelo2.jpg"},
+        {"nome": "Modelo 3", "descricao": "Descrição do modelo 3", "imagem": "modelo3.jpg"},
+    ]
--- app/rote/home.py
+++ app/rote/home.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, render_template
+from app.db.index import index_page, perfil_modelos
 
 home_bp = Blueprint('home', __name__)
 
+HOME_DATA = index_page()
+MODELOS_DATA = perfil_modelos()
+
+
 @home_bp.route('/')
 def index():
-    return render_template('pages/index.html', titulo='Home')
+    return render_template('pages/index.html', home=HOME_DATA, modelos=MODELOS_DATA)
